d8145c99d6cf719827a01083ebeaf5624c61ae78
Hamzaakl <hamzakl0663@gmail.com>
2025-05-26 12:40:43 +0300

    dosya yapısı kuruluyor

---
+++ backend/models/Order.js
@@ -0,0 +1,29 @@
+const mongoose = require("mongoose");
+
+const orderSchema = new mongoose.Schema({
+  userId: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
+  products: [
+    {
+      productId: { type: mongoose.Schema.Types.ObjectId, ref: "Product" },
+      name: String,
+      price: Number,
+      quantity: Number,
+    },
+  ],
+  totalPrice: { type: Number, required: true },
+  address: { type: String, required: true },
+  status: {
+    type: String,
+    enum: ["pending", "paid", "shipped", "delivered"],
+    default: "pending",
+  },
+  paymentStatus: {
+    type: String,
+    enum: ["success", "fail", "pending"],
+    default: "pending",
+  },
+  paymentId: { type: String },
+  createdAt: { type: Date, default: Date.now },
+});
+
+module.exports = mongoose.model("Order", orderSchema);
